527eabfc5d1b0f6c59bb95ca94668bdf09a3a2b3
alexbeatnik <alexbeatnik@gmail.com>
2023-03-20 09:00:57 +0200

    Add second test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -5,3 +5,11 @@ test('Example', async ({ page }) => {
     const pageTitle = await page.locator('h1');
     await expect(pageTitle).toHaveText('Example Domain');
 });
+
+test('Cliking on elements', async ({ page }) => {
+    await page.goto('http://zero.webappsecurity.com/index.html');
+    await page.click('#signin_button');
+    await page.click('text=Sign in');
+    const errorMessage = await page.locator('.alert-error');
+    await expect(errorMessage).toHaveText('Login and/or password are wrong.');
+});
