08a410c82cc16b666b4980674d6db7ba90c4a67d
alexbeatnik <alexbeatnik@gmail.com>
2023-04-01 08:38:13 +0300

    Add search test

---
+++ tests/e2e/e2e-search.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Search result', () => {
+    test('Should find search results', async ({ page }) => {
+        await page.goto('http://zero.webappsecurity.com/');
+        await page.type('#searchTerm', 'bank');
+        await page.keyboard.press('Enter');
+        const numberOfLinks = await page.locator('li > a');
+        await expect(numberOfLinks).toHaveCount(2);
+    });
+});
